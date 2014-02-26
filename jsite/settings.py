3abf2b383f2ebcb7541e881384a30f609d101531
jcchen <junchangchen@gmail.com>
2014-02-27 01:20:57 +0800

    add static resources(bootstrap etc.); modify templates

--- jsite/settings.py
+++ jsite/settings.py
@@ -74,7 +74,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, "static")
+STATIC_ROOT = '/srv/www/static'  # os.path.join(PROJECT_PATH, "static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -85,6 +85,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, "static"), 
 )
 
 # List of finder classes that know how to find static files in
@@ -217,6 +218,6 @@ LOGGING = {
 
 
 CMS_TEMPLATES = (
-    ('template_1.html', 'Template One'),
-    ('template_2.html', 'Template Two'),
+    ('tpl_main.html', 'Template main'),
+    # ('template_2.html', 'Template Two'),
 )
